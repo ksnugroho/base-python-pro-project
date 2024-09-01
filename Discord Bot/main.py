13218f3ddab7e32c6ae898445bf2994aa9d6d7fa
Kuncahyo Setyo Nugroho <ksnugroho26@gmail.com>
2024-09-01 14:33:22 +0700

    UPDATE

---
+++ Discord Bot/main.py
@@ -0,0 +1,39 @@
+# import library
+import discord
+from discord.ext import commands
+
+# import code custom yang sudah kita buat
+from model import get_class
+from model import get_duck_image_url
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command()
+async def cek_gambar(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            file_url = attachment.url
+            await attachment.save(f"./{attachment.filename}")
+            await ctx.send(f"Menyimpan gambar ke ./{attachment.filename}")
+            await ctx.send(get_class(model_path="./keras_model.h5", labels_path="labels.txt", image_path=f"./{attachment.filename}"))
+    else:
+        await ctx.send("Anda lupa mengunggah gambar :(")
+
+bot.run("MTI2NjYwMDI4OTk3MzMwNTM4NQ.GhZZhH.nx0suv2puE7_Tnpx7LMN_UVfer38UwSAVyV7gU")
---
+++ Discord Bot/speech.py
@@ -0,0 +1,14 @@
+import speech_recognition as speech_recog
+
+#### V2 - script
+mic = speech_recog.Microphone()
+recog = speech_recog.Recognizer()
+
+with mic as audio_file:
+    print("Silakan bicara")
+
+    recog.adjust_for_ambient_noise(audio_file)
+    audio = recog.listen(audio_file)
+
+    print("Mengkonversikan ucapan menjadi Teks...")
+    print("Kamu berkata: " + recog.recognize_google(audio, language="id-ID"))
